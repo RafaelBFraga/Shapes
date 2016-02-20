73ad15f16e76e61496a615b90b035250ab8fed6d
Rafael Fraga <rfraga@thoughtworks.com>
2016-02-20 15:00:36 -0200

    Added  dot test to Triangle

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] args) {
 
         Triangle triangle = new Triangle();
-//        System.out.println(triangle.drawRightTriangle(3));
+        System.out.println(triangle.drawRightTriangle(3));
         Diamond diamond = new Diamond();
         System.out.println(diamond.drawDiamondwithName(3));
     }
--- src/test/java/TriangleTest.java
+++ src/test/java/TriangleTest.java
@@ -1,5 +1,32 @@
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.*;
 
+/**
+ * Created by rfraga on 2/20/16.
+ */
 public class TriangleTest {
 
+    @Test
+    public void ShouldDrawDot() throws Exception {
+        Triangle triangle = new Triangle();
+        String expectedDot = "*";
+        assertThat(triangle.drawDot(),is(expectedDot));
+    }
+
+    @Test
+    public void testDrawLine() throws Exception {
+
+    }
+
+    @Test
+    public void testDrawVertical() throws Exception {
+
+    }
+
+    @Test
+    public void testDrawRightTriangle() throws Exception {
+
+    }
 }
